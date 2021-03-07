c5f53c07e8cf89bd5319fa4dee8d036907d3b633
Andrew Kaiser <andrew.kaiser@wbgames.com>
2021-03-07 12:53:08 -0500

    (feat): duration parser supports parens

--- text_parsers.ts
+++ text_parsers.ts
@@ -52,66 +52,55 @@ function parse_unit<T = number, U = number, V = number>(
 // "00:00:00.0000 - 00:00:00"
 // "00:00:03.0000 - 00:00:01.1 - 00:00:00.5"
 // "00:00:03.0000 + {CLIP_0.trim.start}"
-const duration_var_regex = /\{[a-zA-Z0-9._-]+\}/
+const duration_var_regex = /\{([a-zA-Z0-9._-]+)\}/
+const parens_regex = /^\(.*?\)/
 function parse_duration(duration_expr: string, template: TemplateParsed): Seconds {
   try {
-    const [duration, operator, ...expr] = duration_expr
-      .trim()
-      .split(' ')
-      .map(s => s.trim())
-    let duration_in_seconds = 0
-    if (duration_var_regex.test(duration)) {
-      const duration_var = duration.substring(1, duration.length - 1)
-      const [clip_id, ...field] = duration_var.split('.')
-      const clip = template.clips.find(c => c.id === clip_id)
-      if (clip) {
-        const duration_field_hopefully = field.reduce((obj: any, key) => obj[key], clip)
-        // TODO include probed durations eventually
-        if (duration_field_hopefully) {
-          duration_in_seconds = parse_duration(duration_field_hopefully, template)
-        } else {
-          throw new InputError(
-            `Invalid duration "${duration}". Specified clip field for clip "${clip_id}" is undefined`
-          )
-        }
-      } else throw new InputError(`Invalid duration "${duration}". Cannot find clip "${clip_id}"`)
+    let current_duration_expr = duration_expr.trim()
+    const [parens_expr] = current_duration_expr.match(parens_regex) ?? [null]
+    let duration_lhs: number = -1
+    let operator = null
+    let expr_rhs: string[] = []
+    if (parens_expr) {
+      const duration_lhs_expr = current_duration_expr.substring(1, parens_expr.length - 1)
+      duration_lhs = parse_duration(duration_lhs_expr, template)
+      ;[operator, ...expr_rhs] = current_duration_expr.substr(parens_expr.length).trim().split(' ').map(s => s.trim())
     } else {
-      const duration_split = duration.split(':')
-      if (duration_split.length !== 3) throw new InputError(`Invalid duration "${duration}". Cannot parse`)
-      // support vlc millisecond notation as well (00:00:00,000)
-      const [hours, minutes, seconds] = duration_split.map(v => parseFloat(v.split(/,|\./).join('.')))
-      duration_in_seconds = hours * 60 * 60 + minutes * 60 + seconds
-    }
-    // const duration_split = duration.split(':')
-    // if (duration_split.length !== 3) throw new InputError(`Invalid duration "${duration_expr}". Cannot parse`)
-
-    // // support vlc millisecond notation as well (00:00:00,000)
-    // const [hours, minutes, seconds] = duration_split.map(v => parseFloat(v.split(/,|\./).join('.')))
-    // const duration_in_seconds = hours * 60 * 60 + minutes * 60 + seconds
-    if (operator) {
-      if (operator && expr.length) {
-        switch (operator) {
-          case '+':
-            return duration_in_seconds + parse_duration(expr.join(' '), template)
-          case '-':
-            return duration_in_seconds - parse_duration(expr.join(' '), template)
-          case '/':
-            return duration_in_seconds / parse_duration(expr.join(' '), template)
-          case '*':
-            return duration_in_seconds * parse_duration(expr.join(' '), template)
-          default:
-            throw new InputError(
-              `Invalid duration "${duration_expr}". Expected "+,-,/,*" where "${operator}" was`
-            )
+      let duration_lhs_expr = ''
+      ;[duration_lhs_expr, operator, ...expr_rhs] = current_duration_expr.split(' ').map(s => s.trim())
+      const [_,variable_expr] = duration_lhs_expr.match(duration_var_regex) ?? []
+      if (variable_expr) {
+        const [clip_id, ...fields] = variable_expr.split('.')
+        const clip = template.clips.find(c => c.id === clip_id)
+        if (clip) {
+          const duration_field_hopefully = fields.reduce((obj: any, key) => obj[key], clip)
+          if (duration_field_hopefully) duration_lhs = parse_duration(duration_field_hopefully, template)
+          else throw new InputError(`Invalid duration "${duration_lhs_expr}". Specified clip field for clip "${clip_id}" is undefined`)
         }
       } else {
-        throw new InputError(
-          `Invalid duration "${duration_expr}". Expected <duration> <operator> <duration_expr>`
-        )
+        duration_lhs = duration_lhs_expr.split(':').reverse().reduce((acc, s,i) => acc+parseFloat(s)*Math.pow(60,i), 0)
       }
-    } else {
-      return duration_in_seconds
     }
+    if (operator) {
+      if (operator && expr_rhs.length) {
+        switch(operator) {
+            case '+':
+              return duration_lhs + parse_duration(expr_rhs.join(' '), template)
+            case '-':
+              return duration_lhs - parse_duration(expr_rhs.join(' '), template)
+            case '/':
+              return duration_lhs / parse_duration(expr_rhs.join(' '), template)
+            case '*':
+              return duration_lhs * parse_duration(expr_rhs.join(' '), template)
+            default:
+              throw new InputError(`Invalid duration "${duration_expr}". Expected "+,-,/,*" where "${operator}" was`)
+        }
+      } else {
+        throw new InputError(`Invalid duration "${duration_expr}". Expected <duration> <operator> <duration_expr>`)
+      }
+    }
+
+    return duration_lhs
   } catch (e) {
     if (e.name === 'TypeError') {
       throw new InputError(`Invalid duration "${duration_expr}". Cannot parse`)
