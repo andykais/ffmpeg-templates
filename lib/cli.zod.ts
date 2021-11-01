0d75be30918097c501a3cd4db7546a16de4fe7b9
Andrew Kaiser <andrew.kaiser@wbgames.com>
2021-11-01 08:17:24 -0400

    output to cwd

--- lib/cli.zod.ts
+++ lib/cli.zod.ts
@@ -17,7 +17,7 @@ function parse_cli_args(deno_args: string[]) {
   const positional_args = args._.map((a) => a.toString())
   const template_filepath = positional_args[0]
   const { dir, name } = path.parse(template_filepath)
-  let output_folder = path.join(dir, 'ffmpeg-templates-projects', `${name}`)
+  let output_folder = path.join(Deno.cwd(), 'ffmpeg-templates-projects', `${name}`)
   if (positional_args[1]) output_folder = positional_args[1]
   return {
      template_filepath,
