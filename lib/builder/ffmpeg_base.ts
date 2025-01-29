6c264a29ae247c87405e549f47d03cea9864956b
Andrew Kaiser <kaisea.rpi@gmail.com>
2025-01-29 13:18:13 -0500

    debug: temp log ffmpeg output

--- lib/builder/ffmpeg_base.ts
+++ lib/builder/ffmpeg_base.ts
@@ -68,7 +68,7 @@ export abstract class FfmpegBuilderBase {
   }
 
   public constructor(protected context: Context) {
-    this.verbosity_flag = this.context.debug ? 'info' : 'error'
+    this.verbosity_flag = 'debug'
   }
 
   public clip_count() { return this.clip_data.length }
